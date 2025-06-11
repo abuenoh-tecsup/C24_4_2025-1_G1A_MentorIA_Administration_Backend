86cd57a876528ad0c5fe2a14861dd64afc885bf2
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-11 01:08:29 -0500

    feat(forums): agrega modelos para foros y comentarios

--- src/forums/models.py
+++ src/forums/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Forum(models.Model):
+    module = models.ForeignKey('courses.Module', on_delete=models.CASCADE, related_name='forums')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_forums')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'forum'
+        verbose_name = 'Foro'
+        verbose_name_plural = 'Foros'
+        ordering = ['-creation_date']
+    
+    def __str__(self):
+        return f"{self.title} - {self.module}"
+
+class Comment(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'comment'
+        verbose_name = 'Comentario'
+        verbose_name_plural = 'Comentarios'
+        ordering = ['creation_date']
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.forum.title}"
