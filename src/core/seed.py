696c64c72d30a0a83f1983f99c191dbc0ce5840d
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-15 23:24:21 -0500

    fix(core): corrige flujo del comando seed para poblar datos

--- src/core/seed.py
+++ src/core/seed.py
@@ -4,10 +4,12 @@ from core.seeders.courses import seed_courses
 from core.db_utils import reset_auto_increment_all_apps
 
 def run_seed():
+    # Resetea los AUTO_INCREMENT de todas las tablas
+    reset_auto_increment_all_apps()
+    
     # Ejecuta los seeders de cada app
     seed_academic()
     seed_authentication()
     seed_courses()
     
-    # Resetea los AUTO_INCREMENT de todas las tablas
-    reset_auto_increment_all_apps()
+    
