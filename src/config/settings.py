7cfae2d1d33b3f73b831667461395c6b29d83763
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-06-15 19:40:37 -0500

    chore: desactiva paginación en DRF

--- src/config/settings.py
+++ src/config/settings.py
@@ -74,8 +74,8 @@ REST_FRAMEWORK = {
         #'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    #'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
